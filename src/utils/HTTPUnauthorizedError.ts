3f99f3240479a1e27cd195cac5ecbfb5238df4e2
Nico Wagner <info@ni-wa.de>
2023-04-20 23:23:18 +0200

    updated 401 error with better custom error message support

--- src/utils/HTTPUnauthorizedError.ts
+++ src/utils/HTTPUnauthorizedError.ts
@@ -1,17 +1,27 @@
 import { HttpError, HttpExceptions } from "@kopf02/express-utils";
 import { Request } from "express";
 
+export interface HttpExceptionUnauthorizedInterface {
+  req: Request;
+  msg?: { title?: string; description?: string };
+  requiredPermission?: any;
+}
+
 class Unauthorized extends HttpExceptions.HttpException {
-  private readonly req: Request;
-  constructor(req: Request) {
-    super(401, "Unauthorized");
-    this.req = req;
+  private readonly obj: HttpExceptionUnauthorizedInterface;
+  constructor(obj: HttpExceptionUnauthorizedInterface) {
+    super(401, obj.msg?.title || "Unauthorized");
   }
-  getBody(): HttpError<string> {
+  getBody(): HttpError<{ path: string; requiredPermission?: any }> {
     return {
       ...super.getBody(),
-      error: "No permission found for accessing this resource",
-      errorDetails: this.req.path,
+      error:
+        this.obj.msg?.description ||
+        "No permission found for accessing this resource",
+      errorDetails: {
+        path: this.obj.req.path,
+        requiredPermission: this.obj.requiredPermission,
+      },
     };
   }
 }
